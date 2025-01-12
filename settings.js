4538fd780f7b46e822e7a10e020f948e93d9e7c9
Feranmi895 <abrahamboluwatife48@gmail.com>
2025-01-12 17:59:27 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const version = require("@whiskeysockets/baileys/package.json").version
 
 
 //Input number for pair code
-global.pairingNumber = "2349019529423"
+global.pairingNumber = "2349073521743"
 
 
 
@@ -20,9 +20,9 @@ global.autoReadChat = false
 global.alwaysonline = true
 global.autoswview = false
 global.public = true
-global.autoreact = false
-global.antidelete = false
-global.autotyping = false
+global.autoreact = true
+global.antidelete = true 
+global.autotyping = true
 global.autoBio = true
 global.antiSpam = true
 global.sign = "𓃵"
